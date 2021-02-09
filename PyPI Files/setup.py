ca584d8da13d9e1ca29c1f38521d2b55f25735c6
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2021-02-08 20:41:12 -0500

    Add files via upload

---
+++ PyPI Files/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+ld = None
+
+setup(
+    name = 'image-modifier',
+    version = '3.0',
+    license = 'LPPL-1.3c',
+    description = 'A python library for modifying .jpg images using PIL',
+    long_description = ld,
+    author = 'RandomKiddo',
+    author_email = 'nghugare2@outlook.com',
+    url = 'https://github.com/RandomKiddo/image-modifier',
+    download_url = 'https://github.com/RandomKiddo/image-modifier/archive/v3.0.tar.gz',
+    keywords = ['PYTHON', 'IMAGE', 'FILTER'],
+    install_requires = [
+        'wheel',
+        'warnings',
+        'PIL'
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: LaTeX Project Public License v1.3c',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
