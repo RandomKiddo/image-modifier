12205e800e70649777eb6a343560c22b5bf76b6e
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2021-02-08 20:50:06 -0500

    Add files via upload

--- PyPI Files/setup.py
+++ PyPI Files/setup.py
@@ -19,16 +19,10 @@ setup(
     url = 'https://github.com/RandomKiddo/image-modifier',
     download_url = 'https://github.com/RandomKiddo/image-modifier/archive/v3.0.tar.gz',
     keywords = ['PYTHON', 'IMAGE', 'FILTER'],
-    install_requires = [
-        'wheel',
-        'warnings',
-        'PIL'
-    ],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: LaTeX Project Public License v1.3c',
         'Programming Language :: Python :: 3.8'
     ]
 )
